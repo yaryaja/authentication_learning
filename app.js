d57f2e4f3d707e74471974c827981609869e2115
ajay <ajayrayjnvp@gmail.com>
2023-05-29 16:05:47 +0530

    second level of encryption added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -11,10 +12,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+const secret="thisismysecretlittleone"
+const encKey = process.env.SOME_32BYTE_BASE64_STRING;
+userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password'] });
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
